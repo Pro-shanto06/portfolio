ef09f71d8c9edbd513c179ab5747731632ad835f
Pro-shanto06 <proshantosaha1999@gmail.com>
2024-01-28 03:37:04 +0600

    header almost done

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__contaner">
+        <h5> Hello I'm</h5>
+        <h1>Proshanto Saha</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA/>
+
+        
+
+      </div>
+      
+        
+      
+    </header>
+  )
+}
+
+export default Header
