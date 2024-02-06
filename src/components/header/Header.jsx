7c6bb11b82d6fd9b2532bf46f6fb23aab20e297c
Pro-shanto06 <proshantosaha1999@gmail.com>
2024-02-06 22:18:32 +0600

    full responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,37 @@
 import React from 'react'
 import './header.css'
-import CTA from './CTA'
+import { FaArrowCircleDown } from "react-icons/fa";
+import { IoCloudDownloadOutline } from "react-icons/io5";
+import CV from '../../assets/cv.pdf'
 import ME from '../../assets/me.png'
 import HeaderSocial from './HeaderSocials'
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        
+
         <div className="header__content">
-        <h5> Hello I'm</h5>
-        <h1>Proshanto Saha</h1>
-        <h5>Fullstack Developer</h5>
-        <p>Experienced software developer skilled in building web applications using React, Angular, and Node.js. Proficient in languages like C++, JavaScript, and Python. Known for solving problems with attention to detail and working collaboratively to create effective solutions.</p>
-        <div className='header__icons'>
-        <CTA/>
-        <HeaderSocial />
+          <h5> Hello I'm</h5>
+          <h1>Proshanto Saha</h1>
+          <h5>Fullstack Developer</h5>
+          <p>Experienced software developer skilled in building web applications using React, Angular, and Node.js. Proficient in languages like C++, JavaScript, and Python. Known for solving problems with attention to detail and working collaboratively to create effective solutions.</p>
+          <div className='header__icons'>
+            <div className='cta'>
+              <a href={CV} download className='btn btn-primary'><IoCloudDownloadOutline />My Resume</a>
+              <a href="#footer" className='btn'>
+                <FaArrowCircleDown /> Scroll Down
+              </a>
+            </div>
+            <HeaderSocial />
+          </div>
         </div>
-        </div>
-        
+
         <div className="me">
           <img src={ME} alt="me" />
         </div>
-        
+
       </div>
+      
     </header>
   )
 }
